241d746894c033aa4d99f449dcacd74b4fc5684e
Adam Zahradník <adam@zahradnik.xyz>
2025-08-16 12:39:33 +0200

    chore: some minor style changes

--- {{project_name}}/{{project_name}}/styles/src/app.js
+++ {{project_name}}/{{project_name}}/styles/src/app.js
@@ -1,6 +1,6 @@
-import htmx from 'htmx.org'
+import htmx from "htmx.org"
 import { Application } from "@hotwired/stimulus"
-import { definitions } from 'stimulus:./controllers'
+import { definitions } from "stimulus:./controllers"
 
 window.htmx = htmx
 
